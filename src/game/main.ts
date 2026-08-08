581df13eb04ba8a5989d21256eaeb05adbe17100
sofisando <sofisandobal9@gmail.com>
2026-08-07 23:43:09 -0300

    pixelArt: true

--- src/game/main.ts
+++ src/game/main.ts
@@ -6,13 +6,13 @@ import { AUTO, Game } from "phaser";
 import { Preloader } from "./scenes/Preloader";
 import { Pasillo } from "./scenes/Pasillo";
 
-
 //  Find out more information about the Game Config at:
 //  https://docs.phaser.io/api-documentation/typedef/types-core#gameconfig
 const config: Phaser.Types.Core.GameConfig = {
   type: AUTO,
   width: 800,
   height: 600,
+  pixelArt: true,
   physics: {
     default: "arcade",
     arcade: {
